0c64a14995e463583bb2a7d6030ebea2227a59e7
ganesh <jensendavid007@outlook.com>
2023-12-02 02:11:14 +0530

    api created

--- index.js
+++ index.js
@@ -20,7 +20,7 @@
 //   * Content of the file should be the current timestamp
 
 const now = new Date();
-console.log(now)
+console.log(now);
 const currentYear = now.getFullYear();
 
 const currentMonth = now.getMonth(); // January is 0, February is 1, and so on
@@ -34,13 +34,26 @@ const currentMinutes = now.getMinutes();
 const currentSeconds = now.getSeconds();
 
 const formattedDate = `${currentYear}-${currentMonth + 1}-${currentDay}`;
-const formattedTime =  `${currentHour}:${currentMinutes}:${currentSeconds}`;
-
+const formattedTime = `${currentHour}:${currentMinutes}:${currentSeconds}`;
 
 import fs from "node:fs";
+import path from "node:path";
 
-const Current_Timestamp_data = formattedDate +  formattedTime
+const Current_Timestamp_data = formattedDate + formattedTime;
 
 fs.writeFile("current date-time.txt", Current_Timestamp_data, (err) => {
   console.log("completed writing!!");
+});
+
+//  2. Write API endpoint to retrive the text file in that particular folder
+
+const dirPath = "C:DRIVE C1\vscodeTasksNodeJs tasks\node-task-1";
+
+fs.readdir(dirPath, "utf8", function (err, files) {
+  const targetExtension = ".txt"; // Change to the desired file extension
+  const targetFiles = files.filter((file) => {
+    return path.extname(file).toLowerCase() === targetExtension;
+  });
+
+  console.log(targetFiles); // List of filenames with the specified extension
 });
